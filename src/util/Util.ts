61aa536f3142a6a6eebc7fdcc78cb8674a781d6b
IRONM00N <64110067+IRONM00N@users.noreply.github.com>
2021-12-29 14:02:47 -0500

    remove debug code - and hope I didn't break anything

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -110,55 +110,24 @@ export default class Util {
 	 * @returns Whether the two values are equal.
 	 */
 	public static deepEquals<T>(a: unknown, b: T, options?: DeepEqualsOptions): a is T;
-	/* eslint-disable no-console */
 	// eslint-disable-next-line complexity
 	public static deepEquals(a: any, b: any, options?: DeepEqualsOptions): boolean {
 		const { ignoreUndefined = true, ignoreArrayOrder = true } = options ?? {};
 
 		if (a === b) return true;
 		if (typeof a !== "object" || typeof b !== "object") throw new TypeError("Not objects");
-		if ((Array.isArray(a) && !Array.isArray(b)) || (!Array.isArray(a) && Array.isArray(b))) {
-			console.debug("deepEquals: Array vs non-array");
-			return false;
-		}
-		const newA =
-			ignoreArrayOrder && Array.isArray(a) && a.length && typeof a[0] === "string"
-				? [...a].sort((aa, bb) => {
-						if (aa < bb) return -1;
-						if (aa > bb) return 1;
-						return 0;
-				  })
-				: a;
-		const newB =
-			ignoreArrayOrder && Array.isArray(b) && b.length && typeof b[0] === "string"
-				? [...b].sort((aa, bb) => {
-						if (aa < bb) return -1;
-						if (aa > bb) return 1;
-						return 0;
-				  })
-				: b;
+		if ((Array.isArray(a) && !Array.isArray(b)) || (!Array.isArray(a) && Array.isArray(b))) return false;
+		const newA = ignoreArrayOrder && Array.isArray(a) && a.length && typeof a[0] !== "object" ? [...a].sort() : a;
+		const newB = ignoreArrayOrder && Array.isArray(b) && b.length && typeof b[0] !== "object" ? [...b].sort() : b;
 		for (const key in newA) {
 			if (ignoreUndefined && newA[key] === undefined && newB[key] === undefined) continue;
-			if (!(key in newB)) {
-				console.log("======================================");
-				console.dir(newA, { depth: 4 });
-				console.dir(newB, { depth: 4 });
-				return false;
-			}
+			if (!(key in newB)) return false;
 			if (typeof newA[key] === "object" && typeof newB[key] === "object") {
 				if (!this.deepEquals(newA[key], newB[key], { ignoreUndefined, ignoreArrayOrder })) return false;
-			} else if (newA[key] !== newB[key]) {
-				console.log("======================================");
-				console.dir(newA[key], { depth: 4 });
-				console.dir(newA, { depth: 4 });
-				console.dir(newB[key], { depth: 4 });
-				console.dir(newB, { depth: 4 });
-				return false;
-			}
+			} else if (newA[key] !== newB[key]) return false;
 		}
 		return true;
 	}
-	/* eslint-enable no-console */
 
 	/**
 	 * Converts a string in snake_case to camelCase.
